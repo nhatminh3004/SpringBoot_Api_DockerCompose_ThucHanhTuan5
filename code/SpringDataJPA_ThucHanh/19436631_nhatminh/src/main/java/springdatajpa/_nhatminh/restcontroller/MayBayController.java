08089b52845b4dbf9297b920c8ad0ca00228f9c4
nhatminh3004 <99701824+nhatminh3004@users.noreply.github.com>
2022-09-30 17:08:47 +0700

    update

---
+++ code/SpringDataJPA_ThucHanh/19436631_nhatminh/src/main/java/springdatajpa/_nhatminh/restcontroller/MayBayController.java
@@ -0,0 +1,25 @@
+package springdatajpa._nhatminh.restcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import springdatajpa._nhatminh.entity.ChuyenBay;
+import springdatajpa._nhatminh.entity.MayBay;
+import springdatajpa._nhatminh.repository.ChuyenbayRepository;
+import springdatajpa._nhatminh.repository.MayBayRepository;
+
+
+import java.util.List;
+
+@RestController
+public class MayBayController {
+    @Autowired
+    private MayBayRepository mayBays;
+
+    @GetMapping("/cau2")
+    public List<MayBay>  cau2() {
+        return  mayBays.findMayBayTamBayHon10000() ;
+    }
+}
